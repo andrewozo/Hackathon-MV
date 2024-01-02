572aea70b6d98d0c22b105f9752e437d3d685cf0
Andrew <andrewozo123@gmail.com>
2024-01-02 13:54:29 -0500

    Add project files.

---
+++ Hackathon-MV.Server/User.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace Hackathon_MV.Server
+{
+    public class User
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+
+        private string Password { get; set; }
+
+        public string? Email { get; set; }
+
+        public User(string firstName, string lastName, string password, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+            Email = email;
+        }
+
+
+
+    }
+}
