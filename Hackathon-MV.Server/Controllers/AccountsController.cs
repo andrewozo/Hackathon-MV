aef75f4e7259754ef7dc3349028c8f60195d9d12
Andrew <andrewozo123@gmail.com>
2024-01-05 15:27:54 -0500

    Connected Account model to DB

---
+++ Hackathon-MV.Server/Controllers/AccountsController.cs
@@ -0,0 +1,65 @@
+﻿using Hackathon_MV.Server.Services.Accounts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon_MV.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountsService _accountsService;
+
+        public AccountsController(IAccountsService accountsService)
+        {
+            _accountsService = accountsService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<GetAccountDTO>>> GetAllAccounts()
+        {
+            return Ok(await _accountsService.GetAllAccounts());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetAccountDTO>>> GetSingleAccount(int id)
+        {
+            return Ok(await _accountsService.GetAccountById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<GetUserDTO>>>> AddUser(
+            AddAccountDTO newAccount
+        )
+        {
+            return Ok(await _accountsService.AddAccount(newAccount));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<List<GetAccountDTO>>>> UpdateAccount(
+            UpdateAccountDTO updateAccount
+        )
+        {
+            var response = await _accountsService.UpdateAccount(updateAccount);
+
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetAccountDTO>>> DeleteAccount(int id)
+        {
+            var response = await _accountsService.DeleteAccount(id);
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ Hackathon-MV.Server/Migrations/20240105201409_AccountCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hackathon_MV.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNum = table.Column<int>(type: "int", nullable: false),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Class = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
