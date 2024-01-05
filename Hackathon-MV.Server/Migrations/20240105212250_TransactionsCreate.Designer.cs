aba4018eccc0d10e4c3eb20d50187b64cef90818
Andrew <andrewozo123@gmail.com>
2024-01-05 16:23:45 -0500

    connected Transaction Model to Database

--- Hackathon-MV.Server/Migrations/20240105212250_TransactionsCreate.Designer.cs
+++ Hackathon-MV.Server/Migrations/20240105212250_TransactionsCreate.Designer.cs
@@ -3,6 +3,7 @@ using Hackathon_MV.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hackathon_MV.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240105212250_TransactionsCreate")]
+    partial class TransactionsCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
